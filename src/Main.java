7949731799f8fcaa492e1905680b7e13fabc318f
churizzo <114716404+churizzo@users.noreply.github.com>
2024-03-06 22:19:55 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -39,7 +39,7 @@ public class Main {
         int anoAlbum = scanner.nextInt();
         scanner.nextLine();
 
-        System.out.print("Digite o nome do álbum: ");
+        System.out.print("Digite o nome d álbum: ");
         String nomeAlbum = scanner.nextLine();
 
         Album album = new Album();
@@ -54,7 +54,7 @@ public class Main {
 
         scanner.close();
 
-        System.out.print("ta errado");
+        System.out.print("ta erado");
 
     }
 }
